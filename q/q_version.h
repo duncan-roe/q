06d613217806482252c0fde2fabd611197cf3692
Duncan Roe <duncan_roe@optusnet.com.au>
2019-05-11 10:19:39 +1000

    Q version 56.207: Progress TODO item "Reduce number of goto stmts"
    Updated:
    
     q/q_version.h: 56.207
    
     q/xistcs.c: down to 0 goto stmts

--- q/q_version.h
+++ q/q_version.h
@@ -1,3 +1,3 @@
 #ifndef Q_VERSION
-#define Q_VERSION 56.206
+#define Q_VERSION 56.207
 #endif
--- q/xistcs.c
+++ q/xistcs.c
@@ -27,13 +27,8 @@
 #include "c1in.h"
 #include "tabsiz.h"
 
-/* Macros */
-
-#define REREAD_CMD goto msg_read_command
-#define REREAD_CMD_S(x) do {msg = x; REREAD_CMD;} while (0)
-#define READ_NEXT_COMMAND goto ok_command
-
 char *xistics_end_sequence = "\025x\n";
+
 void
 xistcs()
 {
@@ -43,6 +38,7 @@ xistcs()
   int octnum = 0;                  /* Returned value */
   int j, k = 0;                    /* Scratch */
   char *msg;
+  bool ok = true;
 
 /* INTERNAL FUNCTIONS */
 
@@ -100,161 +96,213 @@ xistcs()
 /* END INTERNAL FUNCTIONS */
 
   end_seq = xistics_end_sequence;
-/*
- * On entry, we are back to normal duplex. Don't read commands via
- * Screenedit system, as this is suspect until characteristics are sorted out
- */
-ok_command:
-  msg = "Ok";
-msg_read_command:
-  if (strcmp(msg, "Ok"))
+
+/* On entry, we are back to normal duplex. Don't read commands via
+ * Screenedit system, as this is suspect until characteristics are sorted out */
+
+  for (;;)
   {
-    fprintf(stderr, "%s\r\n", msg);
-    msg = "ER";
-  }                                /* if (strcmp(msg, "Ok")) */
-  printf("%s> ", msg);
-  if (!cl5get((char *)cmdbuf.bdata, BUFMAX, true, true))
-  {
-    cmdbuf.bdata[0] = 'x';
-    cmdbuf.bdata[1] = 0;
-    puts("x");
-  }                                /* if (!cl5get(...)) */
-  cmdbuf.bchars = strlen((char *)cmdbuf.bdata);
-  (void)scrdtk(5, 0, 0, &cmdbuf);
-  (void)scrdtk(1, buf, 2, &cmdbuf);
-  verb = buf[0];
-  if (cmdbuf.toktyp != nortok || cmdbuf.toklen != 1)
-    REREAD_CMD_S("Must have single-char command");
-  switch (verb)
-  {
-    case 'A':                      /* End - of - line */
-      if (!get_result())
-        REREAD_CMD;
-      if (result == 0)
-      {
-        if (!eolok())
-          REREAD_CMD;              /* Check EOL now */
-      }                            /* if (result == 0) */
-      else
-      {
-        p = cachrs;                /* Where characters go */
-        j = result;                /* Result includes backspaces */
-        if (j > PRMAX)
-          j = PRMAX;
-        for (k = j; k > 0; k--)
+    if (ok)
+      msg = "Ok";
+    else
+    {
+      fprintf(stderr, "%s\r\n", msg);
+      msg = "ER";
+      ok = true;
+    }                              /* if (ok) else */
+    printf("%s> ", msg);
+    if (!cl5get((char *)cmdbuf.bdata, BUFMAX, true, true))
+    {
+      cmdbuf.bdata[0] = 'x';
+      cmdbuf.bdata[1] = 0;
+      puts("x");
+    }                              /* if (!cl5get(...)) */
+    cmdbuf.bchars = strlen((char *)cmdbuf.bdata);
+    (void)scrdtk(5, 0, 0, &cmdbuf);
+    (void)scrdtk(1, buf, 2, &cmdbuf);
+    verb = buf[0];
+    if (cmdbuf.toktyp != nortok || cmdbuf.toklen != 1)
+    {
+      msg = "Must have single-char command";
+      ok = false;
+      continue;
+    }
+    switch (verb)
+    {
+      case 'A':                    /* End - of - line */
+        if (!get_result())
         {
+          ok = false;
+          continue;
+        }
+        if (result == 0)
+        {
+          if (!eolok())
+          {
+            ok = false;
+            continue;
+          }
+        }                          /* if (result == 0) */
+        else
+        {
+          p = cachrs;              /* Where characters go */
+          j = result;              /* Result includes backspaces */
+          if (j > PRMAX)
+            j = PRMAX;
+          for (k = j; k > 0; k--)
+          {
 /* Get an octal character to OCTNUM. Single characters stand for themselves,
  * so octal numbers must have at least 2 digits... */
-          if (scrdtk(1, buf, 4, &cmdbuf))
-          {
-            fprintf(stderr, "%s. octno (scrdtk)\r\n", strerror(errno));
-            REREAD_CMD_S("Error - see above");
-          }
-          if (cmdbuf.toktyp != eoltok)
-          {
-            if (cmdbuf.toktyp ^= nortok)
-              REREAD_CMD_S("Null value not allowed");
-            if (cmdbuf.toklen == 1)
-              octnum = buf[0];
-            else
+            if (scrdtk(1, buf, 4, &cmdbuf))
             {
-              if (!cmdbuf.octok)
-                REREAD_CMD_S("Bad octal #"); /* J not octal after all */
-              octnum = cmdbuf.octval;
-              if (!good_octnum())
-                REREAD_CMD;
-            }                      /* if (cmdbuf.toklen == 1) else */
-          }                        /* if (cmdbuf.toktyp != eoltok) */
-          if (cmdbuf.toktyp == eoltok)
-            break;                 /* J out no octal char (EOL) */
-          *p++ = octnum;
-        }                          /* for (k = j; k > 0; k--) */
-        if (!eolok())
-          REREAD_CMD;
-      }                            /* if (result == 0) else */
-      cacnt = result;
-      READ_NEXT_COMMAND;
+              fprintf(stderr, "%s. octno (scrdtk)\r\n", strerror(errno));
+              {
+                msg = "Error - see above";
+                ok = false;
+                break;
+              }
+            }
+            if (cmdbuf.toktyp != eoltok)
+            {
+              if (cmdbuf.toktyp ^= nortok)
+              {
+                msg = "Null value not allowed";
+                ok = false;
+                break;
+              }
+              if (cmdbuf.toklen == 1)
+                octnum = buf[0];
+              else
+              {
+                if (!cmdbuf.octok)
+                {
+                  msg = "Bad octal #";
+                  ok = false;
+                  break;
+                }
+                octnum = cmdbuf.octval;
+                if (!good_octnum())
+                {
+                  ok = false;
+                  break;
+                }
+              }                    /* if (cmdbuf.toklen == 1) else */
+            }                      /* if (cmdbuf.toktyp != eoltok) */
+            if (cmdbuf.toktyp == eoltok)
+              break;               /* J out no octal char (EOL) */
+            *p++ = octnum;
+          }                        /* for (k = j; k > 0; k--) */
+          if (!ok || !eolok())
+          {
+            ok = false;
+            continue;
+          }
+        }                          /* if (result == 0) else */
+        cacnt = result;
+        ok = true;
+        continue;
 
-    case 'B':                      /* Backspace */
-      (void)scrdtk(1, buf, 4, &cmdbuf);
-      if (cmdbuf.toktyp == eoltok)
-        bspace = !bspace;
-      else
-      {
-        if (cmdbuf.toktyp != nortok)
+      case 'B':                    /* Backspace */
+        (void)scrdtk(1, buf, 4, &cmdbuf);
+        if (cmdbuf.toktyp == eoltok)
+          bspace = !bspace;
+        else
         {
-          msg = "Bad param";
-          REREAD_CMD;
-        }                          /* if (cmdbuf.toktyp != nortok) */
+          if (cmdbuf.toktyp != nortok)
+          {
+            msg = "Bad param";
+            ok = false;
+            continue;
+          }                        /* if (cmdbuf.toktyp != nortok) */
 /*
  * If we have been given an octal character, set this as the
  * backspace character from now on, and set bspace true ...
  */
-        verb = buf[0];
-        result = cmdbuf.octok;     /* In case we got an OCTNUM */
-        octnum = cmdbuf.octval;    /* In case we got an OCTNUM */
-        if (!eolok())
-          REREAD_CMD;
-        if (result)
-        {
-          if (!good_octnum())
-            REREAD_CMD;
-          backsp = octnum;
-          bspace = true;
-        }                          /* if (result) */
-        else
-        {
-          switch (verb)
+          verb = buf[0];
+          result = cmdbuf.octok;   /* In case we got an OCTNUM */
+          octnum = cmdbuf.octval;  /* In case we got an OCTNUM */
+          if (!eolok())
           {
-            case 'Y':
-              bspace = true;
-              break;
-            case 'T':
-              bspace = true;
-              break;
-            case 'N':
-              bspace = false;
-              break;
-            case 'F':
-              bspace = false;
-              break;
-            default:
-              msg = "param not recognised";
-              REREAD_CMD;
-          }                        /* switch (verb) */
-        }                          /* if (result) else */
-      }                            /* if (cmdbuf.toktyp == eoltok) else */
-      READ_NEXT_COMMAND;
+            ok = false;
+            continue;
+          }
+          if (result)
+          {
+            if (!good_octnum())
+            {
+              ok = false;
+              continue;
+            }
+            backsp = octnum;
+            bspace = true;
+          }                        /* if (result) */
+          else
+          {
+            switch (verb)
+            {
+              case 'Y':
+                bspace = true;
+                break;
+              case 'T':
+                bspace = true;
+                break;
+              case 'N':
+                bspace = false;
+                break;
+              case 'F':
+                bspace = false;
+                break;
+              default:
+                msg = "param not recognised";
+                ok = false;
+                continue;
+            }                      /* switch (verb) */
+          }                        /* if (result) else */
+        }                          /* if (cmdbuf.toktyp == eoltok) else */
+        ok = true;
+        continue;
 
-    case 'D':                      /* Set or display per-character delay */
-      puts("Delay is not currently working\r");
-      READ_NEXT_COMMAND;
+      case 'D':                    /* Set or display per-character delay */
+        puts("Delay is not currently working\r");
+        ok = true;
+        continue;
 
-    case 'X':                      /* Back to main editor */
-      if (!eolok())
-        REREAD_CMD;                /* Check no params */
-      end_seq = normal_end_sequence;
-      if (simulate_q)
-        simulate_q_idx = 0;
-      return;
+      case 'X':                    /* Back to main editor */
+        if (!eolok())
+        {
+          ok = false;
+          continue;
+        }
+        end_seq = normal_end_sequence;
+        if (simulate_q)
+          simulate_q_idx = 0;
+        return;
 
-    case 'T':                      /* Tab spacing in file */
-      if (!get_result())
-        REREAD_CMD;
-      if (!eolok())
-        REREAD_CMD;
-      tabsiz = result ? result : 8;
-      READ_NEXT_COMMAND;
+      case 'T':                    /* Tab spacing in file */
+        if (!get_result() || !eolok())
+        {
+          ok = false;
+          continue;
+        }
+        tabsiz = result ? result : 8;
+        ok = true;
+        continue;
 
-    case 'W':
-      if (!eolok())
-        REREAD_CMD;
-      vt100 = false;
-      READ_NEXT_COMMAND;
+      case 'W':
+        if (!eolok())
+        {
+          ok = false;
+          continue;
+        }
+        vt100 = false;
+        ok = true;
+        continue;
 
-    default:
-      fprintf(stderr, "%c", verb);
-      msg = " is not a recognised characteristic";
-      REREAD_CMD;
-  }
+      default:
+        fprintf(stderr, "%c", verb);
+        msg = " is not a recognised characteristic";
+        ok = false;
+        continue;
+    }
+  }                                /* for(;;) */
 }                                  /* main() */
